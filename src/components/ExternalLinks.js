b90a49131d2f3dc6a726517dc12b7ba20394fcd0
Brij Bhatia <brijbhatia997@gmail.com>
2026-04-24 18:46:26 +0530

    Updated ExternalLinks

--- src/components/ExternalLinks.js
+++ src/components/ExternalLinks.js
@@ -2,44 +2,29 @@ import React from "react";
 import GitHubIcon from "@material-ui/icons/GitHub";
 import OpenInBrowserIcon from "@material-ui/icons/OpenInBrowser";
 
-class ExternalLinks extends React.Component {
-  constructor() {
-    super();
-    this.state = {
-      expanded: true,
-      activeKey: "1"
-    };
-    this.handleSelect = this.handleSelect.bind(this);
-  }
-  handleSelect(eventKey) {
-    this.setState({
-      activeKey: eventKey
-    });
-  }
-  render() {
-    return (
-      <span className="external-links">
-        <a className="github-icon" href={this.props.githubLink} target="_blank" rel="noopener noreferrer">
-          <GitHubIcon
+const ExternalLinks = ({ githubLink, openLink }) => {
+  return (
+    <span className="external-links">
+      <a className="github-icon" href={this.props.githubLink} target="_blank" rel="noopener noreferrer">
+        <GitHubIcon
+          style={{
+            fontSize: 20,
+            color: "var(--lightest-slate)"
+          }}
+        ></GitHubIcon>
+      </a>
+      {this.props.openLink && (
+        <a className="open-icon" href={this.props.openLink} target="_blank" rel="noopener noreferrer">
+          <OpenInBrowserIcon
             style={{
-              fontSize: 20,
+              fontSize: 25,
               color: "var(--lightest-slate)"
             }}
-          ></GitHubIcon>
+          ></OpenInBrowserIcon>
         </a>
-        {this.props.openLink && (
-          <a className="open-icon" href={this.props.openLink} target="_blank" rel="noopener noreferrer">
-            <OpenInBrowserIcon
-              style={{
-                fontSize: 25,
-                color: "var(--lightest-slate)"
-              }}
-            ></OpenInBrowserIcon>
-          </a>
-        )}
-      </span>
-    );
-  }
+      )}
+    </span>
+  );
 }
 
 export default ExternalLinks;
